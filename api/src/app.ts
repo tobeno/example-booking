57a48b6bf45e8ccae9e0eb98bab11ca66303ddc2
Tobias Laber <tobeno@gmx.de>
2019-09-16 00:24:45 +0200

    Add full CORS support to API

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import path from "path";
+import cors from "cors";
 
 const rootPath = path.resolve(__dirname, "..");
 
 export function createApp(): Application {
   const app = express();
+
+  app.use(cors());
 
   app.use(express.static(`${rootPath}/public`));
 
