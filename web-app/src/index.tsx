30dea9fedd4845eedd7125c67d4bda11fdb38cf9
Tobias Laber <tobeno@gmx.de>
2019-10-12 22:33:28 +0200

    Add service worker to web-app

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -5,6 +5,7 @@ import AppContextProvider from "./components/context/AppContextProvider";
 import App from "./components/App";
 import { apiClient } from "./services";
 import theme from "./theme";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <AppContextProvider apiClient={apiClient} theme={theme}>
@@ -12,3 +13,5 @@ ReactDOM.render(
   </AppContextProvider>,
   document.getElementById("root"),
 );
+
+serviceWorker.register();
